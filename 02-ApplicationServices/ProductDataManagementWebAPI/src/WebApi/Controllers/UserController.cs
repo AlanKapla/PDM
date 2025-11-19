170e4f1146e432418b1daf704926dd36c4198d44
Alan Kapla <kapla.alan@gmail.com>
2025-11-19 22:16:03 +0100

    fix refresh token

--- 02-ApplicationServices/ProductDataManagementWebAPI/src/WebApi/Controllers/UserController.cs
+++ 02-ApplicationServices/ProductDataManagementWebAPI/src/WebApi/Controllers/UserController.cs
@@ -41,10 +41,15 @@ public class UserController : BaseApiController
     }
 
     [HttpPost("refresh")]
-    public async Task<IActionResult> RefreshToken([FromBody] UserRefreshQuery request)
+    public async Task<IActionResult> RefreshToken()
     {
+        UserRefreshQuery request = new()
+        {
+            RefreshToken = cookieService.GetRefreshToken() ?? string.Empty
+        };
+
         UserAuthWeb userAuthWeb = await Send(request);
-        
+
         cookieService.SetAccessToken(userAuthWeb.AccessToken, userAuthWeb.AccessTokenExpiresAt);
         cookieService.SetRefreshToken(userAuthWeb.RefreshToken, userAuthWeb.RefreshTokenExpiresAt);
 
